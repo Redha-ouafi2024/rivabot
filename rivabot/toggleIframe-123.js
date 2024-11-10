e0cc56e9a48b8828b2e23d8dd27eee6e52b1754e
Redha-ouafi2024 <tonyredharussian@gmail.com>
2024-11-10 04:49:11 -0800

    Add files via upload

---
+++ rivabot/toggleIframe-123.js
@@ -0,0 +1,8 @@
+        function toggleIframeee() {
+            const iframe = document.getElementById("news-container");
+            if (iframe.style.display === "none") {
+                iframe.style.display = "block";
+            } else {
+                iframe.style.display = "none";
+            }
+        }
